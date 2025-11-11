4bf91054ba6c5f8c5f0bccb18164fd0bbcc8e316
sevdokimov1 <s.evdokimov383@gmail.com>
2025-11-11 12:07:01 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
 int main() {
     std::string input;
     
-    std::cout << "Введите строки (Ctrl+D для выхода):" << std::endl;
+    std::cout << "Введите строки (\\q или Ctrl+D для выхода):" << std::endl;
     
     while (true) {
-        
-        std::cout << "> ";
-        
+                std::cout << "> ";
         
         if (!std::getline(std::cin, input)) {
-            
             break;
         }
         
+        if (input == "\\q") {
+            break;
+        }
         
         std::cout << input << std::endl;
     }
