f3c9c73cc2b1d1ee14c5afcabf20b243f7c18cd6
sevdokimov1 <s.evdokimov383@gmail.com>
2025-11-11 11:58:35 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,18 +4,22 @@
 int main() {
     std::string input;
     
+    std::cout << "Введите строки (Ctrl+D для выхода):" << std::endl;
+    
     while (true) {
-        // Чтение строки
-        std::getline(std::cin, input);
         
-        // Печать введенной строки
-        std::cout << input << std::endl;
+        std::cout << "> ";
         
-        // Выход если введено "exit"
-        if (input == "exit") {
+        
+        if (!std::getline(std::cin, input)) {
+            
             break;
         }
+        
+        
+        std::cout << input << std::endl;
     }
     
+    std::cout << "Выход из программы" << std::endl;
     return 0;
 }
